36e70ebc9e9dca5733c69461bbd6735337a3ad6b
jink2005 <jink2005@163.com>
2014-05-18 22:30:27 +0800

    Add for android udp broadcast.

--- Android/src/cn/aiseminar/aisentry/transceiver/PeerManager.java
+++ Android/src/cn/aiseminar/aisentry/transceiver/PeerManager.java
@@ -1,5 +1,10 @@
 package cn.aiseminar.aisentry.transceiver;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -8,11 +13,36 @@ public class PeerManager {
 	private Context mContext = null;
 	private Transceiver mOwner = null;
 	private String mEntryName = null;
+	private MulticastSocket broadcastSocket = null;
+	
+	private static final int BROADCAST_PORT = 45000;
 	
 	public PeerManager(Context context, Transceiver owner)
 	{
 		mOwner = owner;
 		mContext = context;
+		
+		try {
+			broadcastSocket = new MulticastSocket();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void startBroadcasting()
+	{
+		DatagramPacket dataPacket = null;
+		try {
+			broadcastSocket.setTimeToLive(4);
+			byte[] data = "192.168.0.105".getBytes();
+			InetAddress address = InetAddress.getByName("224.0.0.1");
+			System.out.println(address.isMulticastAddress());
+			dataPacket = new DatagramPacket(data, data.length, address, BROADCAST_PORT);
+			broadcastSocket.send(dataPacket);
+			broadcastSocket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public boolean isWifiOnline()
--- Android/src/cn/aiseminar/aisentry/transceiver/Transceiver.java
+++ Android/src/cn/aiseminar/aisentry/transceiver/Transceiver.java
@@ -1,5 +1,14 @@
 package cn.aiseminar.aisentry.transceiver;
 
+import android.content.Context;
+
 public class Transceiver {
+	private Context mContext = null;
+	private PeerManager mPeerManager = null;
 	
+	public Transceiver(Context context)
+	{
+		mContext = context;
+		mPeerManager = new PeerManager(mContext, this);
+	}
 }
